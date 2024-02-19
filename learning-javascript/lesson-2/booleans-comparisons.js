bf9149551debfe4d6b8ce992ff82045eb55ab4cb
darman <dxlacerate@gmail.com>
2024-02-20 04:13:14 +0700

    chore: adding booleans

--- learning-javascript/lesson-2/booleans-comparisons.js
+++ learning-javascript/lesson-2/booleans-comparisons.js
@@ -13,3 +13,21 @@ const result = names.includes('bowser');
 console.log(result);
 
 // comparisons operators
+const age = 25;
+// single equal is used to same
+console.log(`is it the age same as 25 ? ${age == 25}`)
+console.log(`is it the age not 25 ? ${age != 30}`)
+console.log(`is it the age below 20 ? ${age > 20}`)
+console.log(`is it the up to 20  ? ${age < 20}`)
+console.log(`is it the below than 25? ${age <= 20}`)
+console.log(`is it the age up to 25 ? ${age >= 20}`)
+
+let name = 'shaun'
+
+
+console.log(name == 'shaun')
+console.log(name == 'Shaun')
+console.log(name > 'crystal')
+//lower case letter is greater than upper case letter
+console.log(name > 'Shaun')
+console.log(name > 'Crystal')
