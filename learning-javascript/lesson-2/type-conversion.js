ce07ce56da489b8d103e67b53bcc61770a7b2e85
darman <dxlacerate@gmail.com>
2024-02-20 04:45:00 +0700

    chore: adding type conversion

---
+++ learning-javascript/lesson-2/type-conversion.js
@@ -0,0 +1,17 @@
+let score = '100'
+score = Number(score)
+console.log(typeof (score + 1))
+
+let numbers = Number('hello')
+let stringg = String(50)
+console.log(typeof numbers)
+console.log(typeof stringg)
+
+// if any string is empty then it will be 0 and it will be false
+// if any string is not empty then it will be true
+let boolss = Boolean('100')
+console.log(boolss, typeof boolss)
+// so if the value of boolean is any numbers whether is negative or positive it will be truthy then it will be true and if it is 0 then it will be false it called falsy
+// it called truthy and falsy values
+let bools = Boolean(100)
+console.log(bools, typeof bools)
