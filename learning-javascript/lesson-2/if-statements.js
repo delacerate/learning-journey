13d0fa2f099756ff3446fe1cb6ff85c1b4f8bfec
darman <dxlacerate@gmail.com>
2024-02-21 00:15:32 +0700

    chore: adding if statement

--- learning-javascript/lesson-2/if-statements.js
+++ learning-javascript/lesson-2/if-statements.js
@@ -7,4 +7,11 @@ if (age > 20) {
 const ninja = ['shaun', 'darman', 'ryu', 'yoshi']
 if (ninja.length > 3) {
   console.log("that's alot of ninja")
+
+}
+
+const pass = 'password'
+
+if (pass.length >= 8) {
+  console.log('that password is long enough')
 }
