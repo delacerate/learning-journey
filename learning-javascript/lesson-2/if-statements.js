0ae4e07b2ad4ade195e3f6304197914f0a502db9
darman <dxlacerate@gmail.com>
2024-02-21 00:14:04 +0700

    chore: adding if statement

---
+++ learning-javascript/lesson-2/if-statements.js
@@ -0,0 +1,10 @@
+// if statements
+const age = 25
+if (age > 20) {
+  console.log('you are over 20 years old')
+}
+
+const ninja = ['shaun', 'darman', 'ryu', 'yoshi']
+if (ninja.length > 3) {
+  console.log("that's alot of ninja")
+}
