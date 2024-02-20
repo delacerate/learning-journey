4681ba4eb3302db884e18de70600c1f0f8d0eda3
darman <dxlacerate@gmail.com>
2024-02-21 01:14:50 +0700

    chore: adding logical not operators

---
+++ learning-javascript/lesson-2/logical-not.js
@@ -0,0 +1,11 @@
+// logical NOT !
+// the logical NOT operator (!) is not change the value of the variable
+// it change the value to the opposite
+// user is always false
+// so the output will be the opposite of the value
+let user = false
+if (!user) {
+  console.log('you must be logged in to continue')
+}
+console.log(!true)
+console.log(!false)
