b6c7be635250e9050c5b02be64f56d74d0d4ce25
darman <dxlacerate@gmail.com>
2024-02-28 03:57:01 +0700

    chore: adding funtion and methods course

---
+++ learning-javascript/lesson-2/function-methods.js
@@ -0,0 +1,12 @@
+const name = "darman";
+
+// function
+const greet = () => {
+  return `hello, ${name}`;
+};
+let resultOne = greet();
+console.log(resultOne);
+
+// methods
+let resultTwo = name.toUpperCase();
+console.log(resultTwo);
