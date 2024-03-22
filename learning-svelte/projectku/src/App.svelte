8997173faf42a3f5af4d0435b1b18ccc2bcdfd36
darman <dxlacerate@gmail.com>
2024-03-22 11:19:38 +0700

    svelte: feat: adding lesson 5 reactive values

---
+++ learning-svelte/projectku/src/App.svelte
@@ -0,0 +1,42 @@
+<script>
+	let firstName = "darman";
+	let lastName = "hendrik";
+	let beltColour = "item manis";
+	$: fullName = `${firstName} ${lastName}`;
+	$: {
+		console.log(beltColour);
+		console.log(fullName);
+	}
+</script>
+
+<main>
+	<p style="color: {beltColour}">
+		{fullName} - {beltColour} kamu sedang ada di belt ini
+	</p>
+	<input type="text" bind:value={firstName} />
+	<input type="text" bind:value={lastName} />
+	<input type="text" bind:value={beltColour} />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+
